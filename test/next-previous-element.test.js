f14f55abdb02e8eb3fe0c0fd750289faa87cdd23
Simon Miner <simon.miner@gmail.com>
2021-03-27 21:25:49 -0400

    Run linter on test file

--- test/next-previous-element.test.js
+++ test/next-previous-element.test.js
@@ -1,21 +1,21 @@
 const np = require("../lib/next-previous-element");
 
-describe( "getPreviousElement tests", function () {
-test('getPreviousElement is null without node', () => {
-    expect(np.getPreviousElement()).toBe(null);
-});
-test('getPreviousElement is null for single node', () => {
-    const textNode = document.createTextNode("This is a test");
-    expect(np.getPreviousElement(textNode)).toBe(null);
-});
+describe("getPreviousElement tests", function () {
+    test('getPreviousElement is null without node', () => {
+        expect(np.getPreviousElement()).toBe(null);
+    });
+    test('getPreviousElement is null for single node', () => {
+        const textNode = document.createTextNode("This is a test");
+        expect(np.getPreviousElement(textNode)).toBe(null);
+    });
 });
 
-describe( "getNextElement tests", function () {
-test('getNextElement is null without node', () => {
-    expect(np.getNextElement()).toBe(null);
-});
-test('getNextElement is null for single node', () => {
-    const textNode = document.createTextNode("This is a test");
-    expect(np.getNextElement(textNode)).toBe(null);
-});
+describe("getNextElement tests", function () {
+    test('getNextElement is null without node', () => {
+        expect(np.getNextElement()).toBe(null);
+    });
+    test('getNextElement is null for single node', () => {
+        const textNode = document.createTextNode("This is a test");
+        expect(np.getNextElement(textNode)).toBe(null);
+    });
 });
