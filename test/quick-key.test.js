2b41b9d71fbb63d7c6c7fdf6abccd4cb1140648d
Simon Miner <simon.miner@gmail.com>
2021-03-28 20:11:09 -0400

    Add one-time setup for QuickKey unit tests

--- test/quick-key.test.js
+++ test/quick-key.test.js
@@ -1,5 +1,18 @@
-const { TestScheduler } = require("jest");
 const QuickKey = require("../lib/quick-key.js").QuickKey;
+
+// One-time setup.
+beforeAll(() => {
+    document.body.innerHTML = `
+<div id="content">
+    <h1>Page heading</h1>
+    <a href="test1"><Test 1</a>
+    <p>This is a paragraph.</p>
+    <a href="test1"><Test 2</a>
+    <a href="test1"><Test 3</a>
+    <span>Nothing special</span>
+</div>
+`;
+});
 
 describe("QuickKey class tests", function () {
     test('constructor sets properties', () => {
