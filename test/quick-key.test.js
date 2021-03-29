c1fe80a45a8dea1a18bae96d3c2a89af921f7443
Simon Miner <simon.miner@gmail.com>
2021-03-28 21:27:07 -0400

    Add QuickKey.findNodes method and incorporate it into constructor

--- test/quick-key.test.js
+++ test/quick-key.test.js
@@ -1,14 +1,18 @@
 const QuickKey = require("../lib/quick-key.js").QuickKey;
 
 // One-time setup.
+const key = 'a';
+const selector = 'a';
+var quickKey = new QuickKey(key, selector);
+
 beforeAll(() => {
     document.body.innerHTML = `
 <div id="content">
     <h1>Page heading</h1>
-    <a href="test1"><Test 1</a>
+    <a href="test1">Test 1</a>
     <p>This is a paragraph.</p>
-    <a href="test1"><Test 2</a>
-    <a href="test1"><Test 3</a>
+    <a href="test1">Test 2</a>
+    <a href="test1">Test 3</a>
     <span>Nothing special</span>
 </div>
 `;
@@ -16,11 +20,27 @@ beforeAll(() => {
 
 describe("QuickKey class tests", function () {
     test('constructor sets properties', () => {
-        const key = 'a';
-        const selector = 'a';
-        var quickKey = new QuickKey(key, selector);
         expect(quickKey instanceof QuickKey).toBe(true);
         expect(quickKey.key).toBe(key);
         expect(quickKey.selector).toBe(selector);
+        expect(quickKey.nodes.length).toEqual(0);
     });
+    test('findNodes populates nodes list', () => {
+        var nodes = quickKey.findNodes(document.body);
+        expect(nodes.length).toBeGreaterThan(0);
+        nodes.forEach(node => {
+            expect(node.nodeName).toBe(quickKey.selector.toUpperCase());
+        });
+    });
+    test('constructor with root node loads node list', () => {
+        quickKey = new QuickKey(key, selector, document.body);
+        expect(quickKey instanceof QuickKey).toBe(true);
+        expect(quickKey.key).toBe(key);
+        expect(quickKey.selector).toBe(selector);
+        expect(quickKey.nodes.length).toBeGreaterThan(0);
+        quickKey.nodes.forEach(node => {
+            expect(node.nodeName).toBe(quickKey.selector.toUpperCase());
+        });
+    });
+    
 });
