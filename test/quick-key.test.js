9c6355332a6bd48549f29809b9768efa290b9394
Simon Miner <simon.miner@gmail.com>
2021-03-28 20:05:46 -0400

    Add QuickKey.currentNodeIndex property and constructor tests

--- test/quick-key.test.js
+++ test/quick-key.test.js
@@ -1,5 +1,13 @@
-const QuickKey = require("../lib/quick-key.js");
+const { TestScheduler } = require("jest");
+const QuickKey = require("../lib/quick-key.js").QuickKey;
 
 describe("QuickKey class tests", function () {
-    
+    test('constructor sets properties', () => {
+        const key = 'a';
+        const selector = 'a';
+        var quickKey = new QuickKey(key, selector);
+        expect(quickKey instanceof QuickKey).toBe(true);
+        expect(quickKey.key).toBe(key);
+        expect(quickKey.selector).toBe(selector);
+    });
 });
