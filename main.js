2aac9b62b8be8dc08a5dc2f536c2856cbc28a6ea
Simon Miner <simon.miner@gmail.com>
2021-04-05 12:54:16 -0400

    Set up quick keys based on selectors submitted by @dorlync

--- main.js
+++ main.js
@@ -2,16 +2,22 @@
 
 import { QuickKeyManager } from './modules/quick-key-manager.js';
 
+// Keys/values for quick keys and the CSS selectors they match.
 let keyData = {
-    'h': 'h1, h2, h3',
-    'l': 'a',
-    'z': 'foo'
+    // Press h/H to move forward/backward through headings.
+    'h': 'h1, h2, h3, h4, h5, h6',
+    // Press k/K to move forward/backward through links.
+    'k': 'a, [role="link"]',
+    // Press r/R to move forward/backward through page regions and landmarks.
+    'r': ' header, nav, main, footer, [role="region"], [role="banner"], [role="navigation"], [role="main"], [role="contentinfo"], [role="search"]', 
+    // Press f/F to move forward/backward through form controls.
+    'f': 'input, select, button, [role="form"], [role="textbox"], [role="checkbox"], [role="button"]',
+    // Press b/B to move forward/backward through buttons.
+    'b': 'button, input[type="button"], input[type="submit"], input[type="reset"], [role="button"]',
+    // Press l/L to move forward/backward through lists.
+    'l': 'ul, ol, dl, [role="list"]'
 };
 
-// TODO: Replace this function with one that suits your purpose.
-function alertQuickKey(node, e) {
-    alert(e.key + " ==> " + node.nodeText + ":" + node.textContent);
-}
 let qkm = new QuickKeyManager(keyData, document.body);
 
 // Check if the pressed key is a quick key.
