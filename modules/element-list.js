d530d84263724553a6b1c719f6b9f475abcb54c9
Simon Miner <simon.miner@gmail.com>
2021-05-08 23:05:48 -0400

    Add ElementList base class and associated tests

---
+++ modules/element-list.js
@@ -0,0 +1,147 @@
+/**
+ * @class
+ * Base class to represent a list of HTML nodes/elements
+ * matching some condition.
+ */
+
+ "use strict";
+ 
+ export class ElementList {
+
+     /**
+      * @member
+      * List of nodes that that match this element list.
+      */
+    nodes = [];
+
+     /**
+      * @member
+      * Index of the current element in the object's list of nodes.
+      */
+    currentNodeIndex = -1;
+
+     /**
+      * @member
+      * Type of wrap-around when a node is requested before
+      * the first or after the last node in the list.
+      * values include "start" or "end".
+      */
+    wrappedTo = undefined;
+
+    /**
+     * @constructor
+     * @param {String} or {Function} finder - CSS selector or function to build the list of nodes.
+     * @param {Node} rootNode - Root node against which selector is run (optional). (defaults to document body).
+     * @returns {ElementList} - A new instance of the ElementList class.
+     */
+    constructor(finder, rootNode = document.body) {
+        if (finder) {
+            this.nodes = this.findNodes(finder, rootNode);
+        }
+    }
+
+    /**
+     * @method
+     * Finds and returns a list of nodes which matches the specified finder CSS selector or function.
+     * @param {String} or {Function} finder - CSS selector or function to build the list of nodes.
+     * - If this parameter is a Function, it takes the node being processed as an argument and return a boolean
+     *   value indicating whether or not the node belongs in this element list.
+     * @param {Node} rootNode - Root node against which selector is run (optional). (defaults to document body).
+     * @returns {Array[Node]} - List of nodes matching the object's selector.
+     */
+    findNodes(finder, rootNode) {
+        var nodes = [];
+        var finderType = "selector";
+        var selector = finder;
+        // Test all nodes beneath the root node if a function
+        // was passed to build the element list.
+        if (typeof selector != "string") {
+            finderType = "function";
+            selector = '*';
+        }
+
+        rootNode.querySelectorAll(selector).forEach( node => {
+            if (finderType == "selector" || (finderType == "function" && finder(node))) {
+                nodes.push(node);
+            }
+        });
+
+        return nodes;
+    }
+
+    /**
+     * @method
+     * Returns the current node matched by this element list.
+     * @returns {Node} - The current matching node or undefined if there are none
+     * or the list of nodes has not been traversed yet.
+     */
+    currentNode() {
+        if (!this.nodes.length || this.currentNodeIndex < 0) {
+            return;
+        }
+        return this.nodes[this.currentNodeIndex];
+    }
+
+    /**
+     * @method
+     * Returns the next node in the DOM matched by this element list.
+     * @returns {Node} - The next matching node (which could be the first one
+     * if we are at the end of the list), or undefined if there are no matching nodes.
+     */
+    nextNode() {
+        // Clear wrap-around.
+        this.wrappedTo = undefined;
+
+        // Make sure there are matching nodes for this element list.
+        if (!this.nodes.length) {
+            return;
+        }
+
+        // Return this node if it's the only one in the list.
+        if (this.nodes.length == 1) {
+            this.currentNodeIndex = 0;
+        }
+        // Get the next node in the list.
+        else if (this.currentNodeIndex < this.nodes.length - 1) {
+            this.currentNodeIndex += 1;
+        }
+        // Or loop around to to the start of the list if we are at the end.
+        else {
+           this.currentNodeIndex = 0;
+           this.wrappedTo = "start";
+        }
+
+        return this.nodes[this.currentNodeIndex];   
+    }
+
+     /**
+     * @method
+     * Returns the previous node in the DOM matched by this element list.
+     * @returns {Node} - The previous matching node (which could be the last one 
+     * if we are at the start of the list), or null if there are no matching nodes.
+     */
+    previousNode() {
+        // Clear wrap-around.
+        this.wrappedTo = undefined;
+
+        // Make sure there are matching nodes for this element list.
+        if (!this.nodes.length) {
+            return;
+        }
+
+        // Return this node if it's the only one in the list.
+        if (this.nodes.length == 1) {
+            this.currentNodeIndex = 0;
+        }        
+        // Get the previous node in the list.
+        else if (this.currentNodeIndex > 0) {
+            this.currentNodeIndex -= 1;
+        }
+        // Or loop around to to the start of the list if we are at the end.
+        else {
+           this.currentNodeIndex = this.nodes.length - 1;
+           this.wrappedTo = "end";       
+        }
+        return this.nodes[this.currentNodeIndex];   
+    }
+ }
---
+++ test/element-list.test.js
@@ -0,0 +1,126 @@
+const ElementList = require("../modules/element-list.js").ElementList;
+
+// One-time setup.
+const selector = "a";
+var nodeList = new ElementList(selector);
+
+beforeAll(() => {
+    document.body.innerHTML = `
+<div id="content">
+    <h1>Page heading</h1>
+    <a href="test1">Test 1</a>
+    <p>This is a paragraph.</p>
+    <a href="test1">Test 2</a>
+    <a href="test1">Test 3</a>
+    <span>Nothing special</span>
+</div>
+`;
+});
+
+beforeEach(() => {
+    nodeList = new ElementList(selector, document.body);
+});
+
+describe("ElementList class tests", function () {
+    /*
+    test('constructor sets properties', () => {
+        nodeList = new ElementList();
+        expect(nodeList instanceof ElementList).toBe(true);
+        expect(nodeList.nodes.length).toBe(0);
+        expect(nodeList.currentNodeIndex).toBe(-1);
+        expect(nodeList.wrappedTo).toBe(undefined);
+    });
+    test('findNodes populates nodes list', () => {
+        nodeList = new ElementList();
+        var nodes = nodeList.findNodes(selector, document.body);
+        expect(nodes.length).toBeGreaterThan(0);
+        nodes.forEach(node => {
+            expect(node.nodeName).toBe(selector.toUpperCase());
+        });
+    });
+    test('constructor with finder loads node list', () => {
+        expect(nodeList instanceof ElementList).toBe(true);
+        expect(nodeList.nodes.length).toBeGreaterThan(0);
+        nodeList.nodes.forEach(node => {
+            expect(node.nodeName).toBe(selector.toUpperCase());
+        });
+    });
+
+    test('currentNode returns undefined in current node index < 0', () => {
+        nodeList = new ElementList();
+        expect(nodeList.currentNode()).toBe(undefined);
+    });
+    test('currentNode returns current match for each node in the list', () => {
+        for (let index = 0; index < nodeList.nodes.length - 1; index++) {
+            nodeList.nextNode();
+            expect(nodeList.currentNode()).toBe(nodeList.nodes[index]);
+        }
+    });
+    */
+    test('currentNode returns undefined for empty node list', () => {
+        nodeList = new ElementList('test', document.body);
+        expect(nodeList.nodes.length).toEqual(0);
+        expect(nodeList.currentNode()).toBe(undefined);
+    });
+
+    test('nextNode returns first match if current node index < 0', () => {
+        nodeList.currentNodeIndex = -1;
+        expect(nodeList.nextNode()).toBe(nodeList.nodes[0]);
+        expect(nodeList.wrappedTo).toBe(undefined);
+    });
+    test('nextNode returns next match for each node in the list before the last one', () => {
+        for (let index = 0; index < nodeList.nodes.length - 2; index++) {
+            nodeList.currentNodeIndex = index;
+            expect(nodeList.nextNode()).toBe(nodeList.nodes[index + 1]);
+            expect(nodeList.wrappedTo).toBe(undefined);
+        }
+    });
+    test('nextNode returns first match for last node in list', () => {
+        nodeList.currentNodeIndex = nodeList.nodes.length - 1;
+        expect(nodeList.nextNode()).toBe(nodeList.nodes[0]);
+        expect(nodeList.wrappedTo).toBe("start");
+    });
+    test('nextNode returns the only match in the list', () => {
+        nodeList = new ElementList('div', document.body);
+        expect(nodeList.nodes.length).toEqual(1);
+        expect(nodeList.nextNode()).toBe(nodeList.nodes[0]);
+        expect(nodeList.nextNode()).toBe(nodeList.nodes[0]);
+    });
+    test('nextNode returns undefined for empty node list', () => {
+        nodeList = new ElementList('test', document.body);
+        expect(nodeList.nodes.length).toEqual(0);
+        expect(nodeList.nextNode()).toBe(undefined);
+        expect(nodeList.wrappedTo).toBe(undefined);
+    });
+
+    test('previousNode returns last match if current node index < 0', () => {
+        nodeList.currentNodeIndex = -1;
+        expect(nodeList.previousNode()).toBe(nodeList.nodes[nodeList.nodes.length - 1]);
+        expect(nodeList.wrappedTo).toBe("end");
+    });
+    test('previousNode returns previous match for each node in the list after the first one', () => {
+        for (let index = nodeList.nodes.length - 1; index > 0; index--) {
+            nodeList.currentNodeIndex = index;
+            expect(nodeList.previousNode()).toBe(nodeList.nodes[index - 1]);
+            expect(nodeList.wrappedTo).toBe(undefined);
+        }
+    });
+    test('previousNode returns last match for first node in list', () => {
+        nodeList.currentNodeIndex = 0;
+        expect(nodeList.previousNode()).toBe(nodeList.nodes[nodeList.nodes.length - 1]);
+        expect(nodeList.wrappedTo).toBe("end");
+    });    
+    test('previousNode returns the only match in the list', () => {
+        nodeList = new ElementList('div', document.body);
+        expect(nodeList.nodes.length).toEqual(1);
+        expect(nodeList.previousNode()).toBe(nodeList.nodes[0]);
+        expect(nodeList.previousNode()).toBe(nodeList.nodes[0]);
+        expect(nodeList.wrappedTo).toBe(undefined);
+    });
+    test('previousNode returns undefined for empty node list', () => {
+        nodeList = new ElementList('test', document.body);
+        expect(nodeList.nodes.length).toEqual(0);
+        expect(nodeList.previousNode()).toBe(undefined);
+        expect(nodeList.wrappedTo).toBe(undefined);
+    });
+});
