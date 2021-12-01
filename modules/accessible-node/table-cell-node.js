95af656ec0805b9cb82ac040e3ff7adbb2c7d333
Simon Miner <simon.miner@gmail.com>
2021-11-30 20:06:43 -0500

    Add TbbleCellNode class and tests

---
+++ modules/accessible-node/table-cell-node.js
@@ -0,0 +1,53 @@
+/**
+ * @class
+ * Class to encapsulate accessibility data for an HTML table cell node/tag.
+ */
+
+ "use strict";
+
+import { AccessibleNode } from "../accessible-node.js";
+
+ export class TableCellNode extends AccessibleNode {
+
+     /**
+      * @member
+      * Index of this cell in its table's list of rows.
+      */
+     rowIndex = -1;
+
+     /**
+      * @member
+      * Index of this cell in its table's list of colums.
+      */
+      columnIndex = -1;
+
+     /**
+      * @member
+      * Number of rows spanned by this cell.
+      */
+     rowSpan = 1;
+
+     /**
+      * @member
+      * Number of columns spanned by this cell.
+      */
+     columnSpan = 1;
+
+     /**
+      * @member
+      * Text of the heading cell in this cell's table row.
+      */
+     rowHeading = undefined;
+
+     /**
+      * @member
+      * Text of the heading cell in this cell's table column.
+      */
+     columnHeading = undefined;
+
+     /**
+      * @member
+      * Type of this table cell -- either "cell" or "heading".
+      */
+     cellType = 'cell';
+ }
---
+++ test/accessible-node/table-cell.test.js
@@ -0,0 +1,22 @@
+const TableCellNode = require("../../modules/accessible-node/table-cell-node.js").TableCellNode;
+const AccessibleNode = require("../../modules/accessible-node.js").AccessibleNode;
+const { htmlToElement } = require('../test-utils.js');
+
+describe("TableCellNode class tests", function () {
+    test('constructor creates accessible table node', () => {
+        var node = htmlToElement('<table><tr><td></td></tr></table>', 'td');
+        var aNode = new TableCellNode(node);
+        expect(aNode instanceof TableCellNode).toBe(true);
+        expect(aNode instanceof AccessibleNode).toBe(true);
+        expect(aNode.actualNode).toBe(node);
+        expect(aNode.tagName).toBe(node.tagName.toLowerCase());
+        expect(aNode.name).toBe(undefined);
+        expect(aNode.rowIndex).toBe(-1);
+        expect(aNode.columnIndex).toBe(-1);
+        expect(aNode.rowSpan).toBe(1);
+        expect(aNode.columnSpan).toBe(1);
+        expect(aNode.rowHeading).toBe(undefined);
+        expect(aNode.columnHeading).toBe(undefined);
+        expect(aNode.cellType).toBe('cell');
+    });
+});
