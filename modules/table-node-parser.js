dbba952a9b75175f846fd10eec9448c09360bb4c
Simon Miner <simon.miner@gmail.com>
2021-12-02 21:05:18 -0500

    Begin implementing TableNodeParser.parse method

--- modules/table-node-parser.js
+++ modules/table-node-parser.js
@@ -6,6 +6,8 @@
 'use strict';
 
 import { NodeParser } from './node-parser.js';
+import { TableNode } from './accessible-node/table-node.js';
+import { TableCellNode } from './accessible-node/table-cell-node.js';
 
 export class TableNodeParser extends NodeParser {
     /**
@@ -21,4 +23,44 @@ export class TableNodeParser extends NodeParser {
      * to their corresponding TableNode objects.
      */
     static tables = {};
+
+    /**
+     * @method
+     * Parse the contents of the specified accessible node
+     * (which must be have a role of "table") to set up
+     * accessibility and navigational data.
+     * @param {AccessibleNode} aNode - The accessible node associated with the table being parsed.
+     * @returns {TableNode} - The parsed accessible table node.
+     */
+    parse(aNode) {
+        // Make sure that this is a table node.
+        if (aNode.tagName !== 'table' && aNode.role !== 'table') {
+            return;
+        }
+
+        const tableNode = aNode.actualNode;
+        if (TableNodeParser.tables[tableNode] !== undefined) {
+            return TableNodeParser.tables[tableNode];
+        }
+
+        var aTableNode = new TableNode(tableNode, aNode.virtualNode);
+        aTableNode.role = aNode.role;
+        const rows = aNode.actualNode.querySelectorAll('table, [role="table"]');
+        for (var rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+            const cells = rows[rowIndex].querySelectorAll(
+                'th, [role="columnheader"], td, [role="cell"]"'
+            );
+            for (
+                var columnIndex = 0;
+                columnIndex > cells.length;
+                columnIndex++
+            ) {
+                var aTableCellNode = new TableCellNode.new(cells[columnIndex]);
+                aTableCellNode.role = 'cell';
+                aTableCellNode.rowIndex = rowIndex;
+                aTableCellNode.columnIndex = columnIndex;
+                aTableNode[rowIndex][columnIndex] = aTableCellNode;
+            }
+        }
+    }
 }
