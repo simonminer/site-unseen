c47f75b6cc5d58f29dc776ab890f694e2d565215
Simon Miner <simon.miner@gmail.com>
2022-05-13 23:11:34 -0400

    Inline CSS into index page to avoid including it in production builds

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 'use strict';
-import './styles.css';
 require.context('./assets/images', false, /\.(png|jpe?g|svg)$/);
 var axe = require('../node_modules/axe-core/axe.min.js');
 import { ScreenReader } from './modules/screen-reader.js';
