3b2eb6a2e209b62b18a689cc1d3bcb61289d25b3
Simon Miner <simon.miner@gmail.com>
2022-06-20 22:00:37 -0400

    Load HTML page content before running Quizzer tests

--- tests/quizzer.test.js
+++ tests/quizzer.test.js
@@ -1,10 +1,23 @@
+const fs = require('fs');
+const { parse } = require('node-html-parser');
+const { htmlToElement } = require('./test-utils.js');
+const ScreenReader = require('../src/modules/screen-reader.js').ScreenReader;
 const Quizzer = require('../src/modules/quizzer.js').Quizzer;
 const Config = require('../src/modules/config.js').Config;
 
 const id = `${Config.cssPrefix}quizzer`;
 
+beforeAll(() => {
+    const html = fs.readFileSync('src/index.html', 'utf8');
+    const rootNode = parse(html).querySelector('main');
+    document.body.innerHTML = rootNode;
+});
+
+var screenReader = undefined;
 var quizzer = undefined;
 beforeEach(() => {
+    screenReader = new ScreenReader();
+    screenReader.setup();
     quizzer = new Quizzer();
 });
 
@@ -12,5 +25,6 @@ describe('Quizzer class tests', function () {
     test('constructor creates quizzer', () => {
         expect(quizzer instanceof Quizzer).toBe(true);
         expect(Quizzer.id).toBe(id);
+        // console.log(screenReader.shortcutKeyManager.shortcutKeys);
     });
 });
